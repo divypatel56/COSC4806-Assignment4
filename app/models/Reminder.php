44b95839939e0bc7a2bd9df265833a301b5b28ce
Divy Patel <67093305+divypatel56@users.noreply.github.com>
2024-06-28 13:57:49 +0000

    Added getAll_reminders function to Reminder model

--- app/models/Reminder.php
+++ app/models/Reminder.php
@@ -1,9 +1,22 @@
 <?php
 
 class Reminder {
+
   public function __construct() {
 
   }
 
-}
+  public function getAll_reminders() {
+    $db = db_connect();
+    $statement = $db->prepare("select * from reminders;");
+    $statement->execute();
+    $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+    return $rows;
+  }
+
+  public function get_reminder($id) {
+    $db = db_connect();
+    //Add Update query
+  }
+}  
 ?>
