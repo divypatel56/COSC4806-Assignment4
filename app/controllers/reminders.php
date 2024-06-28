fc3a32a4366431141d69f7e150a49ee04f2c953a
Divy Patel <67093305+divypatel56@users.noreply.github.com>
2024-06-28 15:18:44 +0000

    Added create reminder function

--- app/controllers/reminders.php
+++ app/controllers/reminders.php
@@ -9,7 +9,15 @@ class Reminders extends Controller {
    }
 
    public function create(){
-     $this->view('reminders/create');
+     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+         $_SESSION['subject'] = $_POST['subject'];
+         $reminder = $this->model('Reminder');
+         $reminder->create_reminder($_SESSION['userid'], $_SESSION['subject']);
+         unset($_SESSION['subject']);
+         header('Location: /reminders');
+     } else {
+         $this->view('reminders/create');
+     }
    }
 
   public function update(){
--- app/models/Reminder.php
+++ app/models/Reminder.php
@@ -18,5 +18,13 @@ class Reminder {
     $db = db_connect();
     //Add Update query
   }
+
+  public function create_reminder($user_id, $subject) {
+      $db = db_connect();
+      $statement = $db->prepare("INSERT INTO reminders (user_id, subject, created_at) VALUES (:user_id, :subject, NOW())");
+      $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+      $statement->bindParam(':subject', $subject, PDO::PARAM_STR);
+      $statement->execute();
+  }
 }  
 ?>
